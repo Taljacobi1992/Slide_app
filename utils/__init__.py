f3343c39883559c7c180cd36becfba93bc20c603
Taljacobi1992 <tal.jacobi1992@gmail.com>
2026-05-04 11:01:47 +0300

    Import slide builder functions in utils init

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 from utils.state import deck_state, get_slide_choices, parse_slide_num_from_selection, get_slide_by_num, detect_slide_count
 from utils.llm import call_llm, call_llm_raw, parse_llm_json
 from utils.revision_manager import RevisionManager
+from utils.slide_builder import build_base_slide_entry, build_content_objects_for_layout
